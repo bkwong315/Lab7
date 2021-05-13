f693d4280b3a237f47eb77ed9e3568ed0af4b6eb
Brian <bkwong@ucsd.edu>
2021-05-12 18:06:27 -0700

    Finished most of Part 1.

--- scripts/router.js
+++ scripts/router.js
@@ -5,7 +5,7 @@ export const router = {};
 /**
  * Changes the "page" (state) that your SPA app is currently set to
  */
-router.setState = function() {
+router.setState = function(newState) {
   /**
    * - There are three states that your SPA app will have
    *    1. The home page
@@ -35,4 +35,80 @@ router.setState = function() {
    *    1. You may add as many helper functions in this file as you like
    *    2. You may modify the parameters of setState() as much as you like
    */
+
+  //history.pushState(newState);
+  const body = document.querySelector('body');
+  const title = document.querySelector('header > h1');
+  const entryPage = document.querySelector('entry-page');
+
+  body.classList.remove('settings');
+  body.classList.remove('single-entry');
+
+  if(newState.type === 'settings') {
+    body.classList.add('settings');
+    title.innerHTML = 'Settings';
+    
+    if(history.state === null || history.state.url !== newState.url) {
+      history.pushState({'type': 'settings'}, newState.title, newState.url);
+    }
+  }
+  else if(newState.type === 'single-entry') {
+    body.removeChild(entryPage);
+    
+    let newEntryPage = document.createElement('entry-page');
+    
+    newEntryPage.entry = newState.entry;
+    body.appendChild(newEntryPage);
+    title.innerHTML = 'Entry ' + newState.url.match(/[0-9]+/g);
+    
+    body.classList.add('single-entry');
+
+    if(history.state === null || history.state.url !== newState.url) {
+      history.pushState(newState, newState.title, newState.url);
+    }
+  }
+  else if(newState.type === 'main') {
+    title.innerHTML = "Journal Entries";
+
+    if(history.state === null || history.state.url !== newState.url) {
+      history.pushState(newState, newState.title, newState.url);
+    }
+  }
+
+  
+  
+  //body.classList.add('single-entry');
+}
+
+window.setTimeout(() => {
+  const journalEntries = document.querySelectorAll('journal-entry');
+
+  //console.log(journalEntries);
+
+  for(let i = 0; i < journalEntries.length; i++) {
+    journalEntries[i].addEventListener('click', () => {
+      router.setState({'type': 'single-entry', 'title': '', 'url': '#entry' + (i + 1), 'entry': journalEntries[i].entry});
+    })
+  }
+  
+}, 500);
+
+const settingsIcon = document.querySelector('header > img');
+const title = document.querySelector('header > h1');
+
+settingsIcon.addEventListener('click', () => {
+  router.setState({'type': 'settings', 'title': '', 'url': '#settings'});
+})
+
+title.addEventListener('click', () => {
+  router.setState({'type': 'main', 'title': '', 'url': ' '});
+})
+
+window.onpopstate = (event) => {
+  if(history.state !== null) {
+    router.setState(history.state);
+  }
+  else {
+    router.setState({'type': 'main', 'title': '', 'url': ' '});
+  }
 }
