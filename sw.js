86b9545f7390bb39ef827af3afb819c033a9df21
Brian <bkwong@ucsd.edu>
2021-05-15 20:48:27 -0700

    Updated urlsToCache

--- sw.js
+++ sw.js
@@ -5,12 +5,26 @@
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
 
+/* Code Adapted from https://developers.google.com/web/fundamentals/primers/service-workers and https://developer.mozilla.org/en-US/docs/Web/API/Clients/claim */
+
 // Activation occurs when the old service worker no longer controls this 
 // webpage. Instead, the new service worker claims the webpage.
 self.addEventListener('activate', event => {
   //console.log('Activated');
   event.waitUntil(clients.claim());
 });
+
+// Initializing cache name and urls to cache.
+let CACHE_NAME = 'journal-cache';
+let urlsToCache = [
+  './index.html',
+  './style.css',
+  './scripts/router.js',
+  './scripts/script.js',
+  './components/entry-page.js',
+  './components/journal-entry.js',
+  'https://cse110lab6.herokuapp.com/entries'
+];
 
 // The install event should trigger after registering the service worker 
 self.addEventListener('install', function(event) {
@@ -23,11 +37,6 @@ self.addEventListener('install', function(event) {
       })
   );
 });
-
-  let CACHE_NAME = 'journal-cache';
-  let urlsToCache = [
-    'https://cse110lab6.herokuapp.com/entries'
-  ];
   
 self.addEventListener('fetch', function(event) {
     //console.log('Fetching');
